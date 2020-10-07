6308272e4a128f0bf76e08ef5fa73307995b40af
Rafael Anderson de Souza <68648004+rafaelandersonsouza@users.noreply.github.com>
2020-10-07 13:19:27 -0300

    Revert "Test imgs"

--- Teste site Tá em Casa/scripts/addField.js
+++ Teste site Tá em Casa/scripts/addField.js
@@ -15,26 +15,9 @@ function cloneField(){
    })
    // Colocar na pagina. Onde?
     document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
 }
 
-let imagens= document.querySelectorAll('.small_img');
-let modal = document.querySelector('.modal');
-let modalImg = document.querySelector('#modal_img');
-let btClose = docmuent.querySelector('#bt_close')
-let srcVal="";
-
-for(let i=0, i<imagens.length; i++){
-    imagens[i].addEventListener('click', function()){
-      
-        srcVal = imagens[i].getAttribute('src');
-        modalImg.setAttribute('src', srcVal);
-        modal.classList.toggle('modal_active');
-    });
-}
-
-btClose.addEventListener('click', function(){
-    modal.classList.toggle('modal_active');
-});
 
 
 
